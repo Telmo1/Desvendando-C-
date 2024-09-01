741ee24312e9afe156797778a81105c95586eda5
Yuri Telmo <yrtelmo00@gmail.com>
2024-09-01 17:35:12 -0300

    Trigonometria e Incrementos

--- Models/Calculadora.cs
+++ Models/Calculadora.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace OperadoresAritimetricos.Models
@@ -23,5 +24,36 @@ namespace OperadoresAritimetricos.Models
         {
             Console.WriteLine($"{x} / {y} = {x / y}");
         }
+
+        public void Potencia (int x, int y)  // Criando função que realiza potenciação 
+        {
+            double pot = Math.Pow(x ,y); // Math é uma biblioteca de operações embutida no C# utilzamos a math para realizar operações mais complexas. 
+            //duble é uma class que representa  números grandes e com virgula se necessario.
+            Console.WriteLine($"{x}^{y} = {pot}"); 
+        }
+
+        public void Seno (double angulo)                    //Agora criei funções que retornam valores trigonometricos (Seno, Coseno e Tangente) utilizando Math
+            {
+                double radiano = angulo * Math.PI / 180;    // Denominamos variavel duble, passamos o angulo, multiplicamos por PI (3,14) e dividimos por 180, para encontrar o radiano
+                double seno = Math.Sin(radiano); // Aqui temos a variavel seno que recebe o valor do radiano e calcula seu Seno
+                Console.WriteLine($"Seno de {angulo} = {Math.Round(seno, 4)}"); // Aqui exibimos o valor do calculo, math.round possibilita exibir a quantidade de casas decimais preferidas.
+            }
+
+            public void Conseno (double angulo)
+            {
+                double radiano = angulo * Math.PI / 180;
+                double coseno = Math.Cos(radiano);
+                Console.WriteLine($"Coseno de {angulo} = {Math.Round (coseno, 4)}");
+            }
+
+            public void Tangente (double angulo)
+            {
+                double radiano = angulo * Math.PI / 180;
+                double tangente = Math.Tan(radiano);
+                Console.WriteLine($"Tangente de {angulo} = {Math.Round(tangente, 4)}"); // O mesmo principio foi utilizado para criar as tres funções apenas mudei a função Math (Sin, Cos e Tan)
+            }
+
+            
+                
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,12 +2,38 @@
 
 Console.WriteLine("Hello, World!");
 
-Calculadora calc = new Calculadora();
+Calculadora calc = new Calculadora(); // Instanciamos uma nova class a ser gerada
+
+//Utilizamos a variavel cal e chamos as funções com o (.) PONTO
 
 calc.Somar(10, 30);
 calc.Subitrair(10, 50);
 calc.Multiplicar(15, 45);
 calc.Dividir(2, 2);
+calc.Potencia(3, 3);
+calc.Seno(30);
+calc.Conseno(30);
+calc.Tangente(30);
+
+// Aprendendo a Incrementar e Decrementar utlizando ++ e --
+
+int numeroI = 10; 
+int numeroD = 20;
+
+Console.WriteLine(numeroI);
+Console.WriteLine(numeroD);
+
+Console.WriteLine("Incrementando o 10");
+Console.WriteLine("Decrementando o 20");
+// numero = numeroI + 1;
+// numero = numeroD - 1;
+
+numeroD--;
+numeroI++;
+
+Console.WriteLine(numeroI);
+Console.WriteLine(numeroD);
+
 
 // Console.WriteLine("Digite uma letra");
 // string letra = Console.ReadLine();
